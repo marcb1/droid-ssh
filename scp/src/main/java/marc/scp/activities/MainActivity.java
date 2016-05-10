bc8f3b33ab3f5d26e28b2df5558ec4195f2cf1c0
Marc Bassil <mbassil@stevens.edu>
2016-05-09 20:16:26 -0400

    testing security provider

--- scp/src/main/java/marc/scp/activities/MainActivity.java
+++ scp/src/main/java/marc/scp/activities/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends Activity
     static
     {
         Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+        calls javax.crypto.Cipher.getInstance.getInstance
         Security.removeProvider("BC");
     }
 
@@ -56,6 +57,17 @@ public class MainActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        try
+        {
+            //this returns provider = "AndroidKeyStoreBCWorkaround version 1.0"
+            // javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("AES/CTR/NoPadding");
+            //this works
+            // cipher = javax.crypto.Cipher.getInstance("AES/CTR/NoPadding", "SC");
+        }
+        catch(Exception e)
+        {
+        }
+
         try
         {
             Class clss = AES256CTR.class;
